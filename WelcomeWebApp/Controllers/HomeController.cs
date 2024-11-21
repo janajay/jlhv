8d76d328aaa522962b1c15bf9fe13923093edf1b
vijayavel_eleviant <vijayavel.rajasekaran@ctg.com>
2024-11-21 15:32:19 +0530

    new asp .net mvc files

---
+++ WelcomeWebApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace WelcomeWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home";
+            ViewData["Message"] = "This is sample ASP. net home page created ny vijayavel R";
+
+            return View();
+        }
+    }
+}
